c02c891993bff6623e69f637c22a500199b7e8ac
HenryGeek <linuxguru@163.com>
2016-06-20 21:06:15 +0800

    try new index.html

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Henry'
 SITENAME = u'Experiment, Explore, Experience'
-SITEURL = ''
+SITEURL = 'https://henrygeek.github.io'
 
 PATH = 'content'
 
@@ -18,6 +18,32 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+THEME = "./voidy-bootstrap"
+SITESUBTITLE ='Sub-title that goes underneath site name in jumbotron.'
+SITETAG = "Text that's displayed in the title on the home page."
+
+# Extra stylesheets, for bootstrap overrides or additional styling.
+STYLESHEET_FILES = ("pygment.css", "voidybootstrap.css",)
+
+# Put taglist at end of articles, and use the default sharing button implementation.
+CUSTOM_ARTICLE_FOOTERS = ("taglist.html", "sharing.html", )
+CUSTOM_SCRIPTS_ARTICLE = "sharing_scripts.html"
+
+# Default sidebar template. Omit this setting for single column mode without sidebar.
+SIDEBAR = "sidebar.html"
+
+SOCIAL = (('Google+', 'http://plus.google.com/userid',
+         'fa fa-google-plus-square fa-fw fa-lg'),
+        ('Twitter', 'https://twitter.com/username',
+         'fa fa-twitter-square fa-fw fa-lg'),
+        ('LinkedIn', 'http://linkedin-url',
+         'fa fa-linkedin-square fa-fw fa-lg'),
+        ('BitBucket', 'http://bitbucket.org/username',
+         'fa fa-bitbucket-square fa-fw fa-lg'),
+        ('GitHub', 'http://github.com/username',
+         'fa fa-github-square fa-fw fa-lg'),
+        )
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
